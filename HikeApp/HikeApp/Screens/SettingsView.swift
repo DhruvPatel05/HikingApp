f725b785f7b03c95b46a52acc2b19ee5dbf8bc4b
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-10 23:00:55 -0700

    list added in Settings screen.

--- HikeApp/HikeApp/Screens/SettingsView.swift
+++ HikeApp/HikeApp/Screens/SettingsView.swift
@@ -9,56 +9,92 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        // MARK: - SECTION: HEADER
-        VStack {
-            HStack {
-                Spacer()
-                Image(systemName: "laurel.leading")
-                    .font(.system(size: 80,weight: .black))
-                
-                VStack(spacing: -10) {
-                    Text("Hike")
-                        .font(.system(size: 66,weight: .black))
-                    Text("Editors' Choice ")
+        List {
+            // MARK: - SECTION: HEADER
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Image(systemName: "laurel.leading")
+                        .font(.system(size: 80,weight: .black))
+                    
+                    VStack(spacing: -10) {
+                        Text("Hike")
+                            .font(.system(size: 66,weight: .black))
+                        
+                        Text("Editors' Choice ")
+                    }
+                    
+                    Image(systemName: "laurel.trailing")
+                        .font(.system(size: 80,weight: .black))
+                    
+                    Spacer()
                 }
-                Image(systemName: "laurel.trailing")
-                    .font(.system(size: 80,weight: .black))
-                Spacer()
-            }
-            .foregroundStyle(LinearGradient(colors:[.customGreenLight,.customGreenMedium,.customGreenDark
-                                                   ],
-                                            startPoint: .top,
-                                            endPoint: .bottom
-                                           )
-            )
-            .padding(.top,8)
-            
-            VStack(spacing:8) {
-                Text("Where can you find \nperfect tracks")
-                    .font(.title2)
-                    .fontWeight(.heavy)
+                .foregroundStyle(
+                    LinearGradient(
+                        colors:[
+                            .customGreenLight,
+                            .customGreenMedium,
+                            .customGreenDark
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .padding(.top,8)
                 
-                Text("The Hike Which looks gorgeous in the photo, but even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app.")
-                    .font(.footnote)
-                    .italic()
-                Text("Dust off the boots! It's time for a walk.")
-                    .fontWeight(.heavy)
-                    .foregroundColor(.customGreenMedium)
+                VStack(spacing:8) {
+                    Text("Where can you find \nperfect tracks")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                    
+                    Text("The Hike Which looks gorgeous in the photo, but even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app.")
+                        .font(.footnote)
+                        .italic()
+                    
+                    Text("Dust off the boots! It's time for a walk.")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.customGreenMedium)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.bottom,16)
+                .frame(maxWidth:.infinity)
             }
-            .multilineTextAlignment(.center)
-            .padding(.bottom,16)
-            .frame(maxWidth:.infinity)
-        }
-        // MARK: - HEADER
-        .listRowSeparator(.hidden)
+            // MARK: - HEADER
+            .listRowSeparator(.hidden)
             // MARK: - SECTION Icons
             
             // MARK: - SECTION About
             
+            Section(
+                header:Text("ABOUT THE APP"),
+                footer: HStack {
+                    Spacer()
+                    Text("Copyright © 2021, All rights reserved.")
+                    Spacer()
+                }
+                .padding(.vertical,8)
+            ) {
+                // 1. Basic Labeled Content
+                // LabeledContent("Application", value: "Hike")
+                // 2. Advance Labeled Content
+                CustomListRowView(
+                    rowLabel: "Application",
+                    rowIcon: "apps.iphone",
+                    rowContent: "HIKE",
+                    rowTintColor: .blue
+                )
+                
+                CustomListRowView(
+                    rowLabel: "Compatibility",
+                    rowIcon: "info.circle",
+                    rowContent: "iOS, iPadOS",
+                    rowTintColor: .red
+                )
+            }//: SECTION
             // MARK: List
-            
         }
-    
+    }
 }
 
 struct SettingsView_Previews: PreviewProvider {
