f907c962e897b6428c725215339d587c505b55ab
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-11 11:43:24 -0700

    Custom list row UI updated.

--- HikeApp/HikeApp/Component/CustomListRowView.swift
+++ HikeApp/HikeApp/Component/CustomListRowView.swift
@@ -11,17 +11,26 @@ struct CustomListRowView: View {
     // MARK: - PROPERTIES
     @State var rowLabel: String
     @State var rowIcon: String
-    @State var rowContent: String
+    @State var rowContent: String? = nil
     @State var rowTintColor: Color
-
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
 
     var body: some View {
            
            // 2. Advance Labeled Content
            LabeledContent {
-               Text(rowContent)
-                   .foregroundColor(.primary)
-                   .fontWeight(.heavy)
+               if rowContent != nil {
+                   Text(rowContent!)
+                       .foregroundColor(.primary)
+                       .fontWeight(.heavy)
+               } else if rowLinkLabel != nil && rowLinkDestination != nil {
+                   Link(rowLinkLabel!,destination: URL(string:rowLinkDestination!)!).foregroundColor(.pink)
+                       .fontWeight(.heavy)
+               }
+               else {
+                   EmptyView()
+               }
            } label: {
                HStack {
                    ZStack {
@@ -44,10 +53,13 @@ struct CustomListRowView_Previews: PreviewProvider {
     static var previews: some View {
         List {
             CustomListRowView(
-                rowLabel: "Designer",
-                rowIcon: "paintpalette",
-                rowContent: "John Doe",
-                rowTintColor: .pink)
+                rowLabel: "Website",
+                rowIcon: "globe",
+                rowContent: "nil",
+                rowTintColor: .pink,
+                rowLinkLabel: "Credo Academy",
+                rowLinkDestination: "https://www.credacademy"
+            )
         }
     }
 }
